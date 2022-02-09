d5fcc8a62873c26b1a56d4fe3ebb0d46c89d94c9
MalikN95 <79022015+MalikN95@users.noreply.github.com>
2022-02-09 18:23:31 +0300

    com

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express()
 const os = require('os')
 
 console.log(os.cpus());
+console.log(os.totalmem());
+console.log(os.freemem());
 console.log('v1.2')
 
 const hbs = exphandle.create({
