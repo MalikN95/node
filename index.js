409cf5f06b371a815065bb168cfde909b3459843
MalikN95 <79022015+MalikN95@users.noreply.github.com>
2022-03-15 18:10:30 +0300

    com

--- index.js
+++ index.js
@@ -1,36 +1,30 @@
 const express = require('express')
 const path = require('path')
-const exphandle = require('express-handlebars')
-const app = express()
-const os = require('os')
+const exphbs = require('express-handlebars')
 const homeRoutes = require('./routes/home')
 const addRoutes = require('./routes/add')
-const aboutRoutes = require('./routes/about')
-const coursesRoutes = require('./routes/coutses')
+const coursesRoutes = require('./routes/courses')
 
-console.log(os.cpus());
-console.log(os.totalmem());
-console.log(os.freemem());
-console.log('v1.2')
+const app = express()
 
-const hbs = exphandle.create({
-    defaultLayout: 'main',
-    extname: 'hbs'
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs'
 })
+
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
 
 app.use(express.static('public'))
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({extended: true}))
+
 app.use('/', homeRoutes)
-app.use('/add',addRoutes)
-app.use('/about',aboutRoutes)
-app.use('/courses',coursesRoutes)
+app.use('/add', addRoutes)
+app.use('/courses', coursesRoutes)
 
-
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-    console.log(`App started is ${PORT} port`);
+  console.log(`Server is running on port ${PORT}`)
 })
